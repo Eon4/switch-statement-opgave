f478650793b70173c20403093811921c7b27bfe1
simo994q <simonkn1@hotmail.com>
2023-02-27 09:44:04 +0100

    opgave2

--- assets/js.js
+++ assets/js.js
@@ -1,6 +1,18 @@
 
 
-// Skriv en switch-statement, der ændrer variablen "today" efter hvilken dag det er i dag.
+// Skriv en switch-statement, der ændrer Inner-HTML'en på "todayText" 
+// efter hvilken dag det er i dag.
 
 const todayText = document.getElementById('today');
-let todayDate = new Date().getDay;
+// Denne variabel angiver dagen det er i dag. Hvis det er mandag, er
+// variablen "0" som værdi, hvis det er tirsdag er den "1" osv.
+let todayDate = new Date().getDay();
+
+
+
+
+
+// Skriv en switch-statement, der ændrer Inner-HTML'en på "todayType" 
+// efter om det i dag er en hverdag eller weekend
+
+const todayType = document.getElementById('type');
