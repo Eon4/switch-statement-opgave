ab02b3ca86f3b8996e51c55d4cbe187c55ddc8b7
simo994q <simonkn1@hotmail.com>
2023-02-27 09:08:26 +0100

    opgave

---
+++ assets/js.js
@@ -0,0 +1,6 @@
+
+
+// Skriv en switch-statement, der ændrer variablen "today" efter hvilken dag det er i dag.
+
+const todayText = document.getElementById('today');
+let todayDate = new Date().getDay;
